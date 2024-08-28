d53fd6eab18afafbe6305be7ab11133d1f2451d5
soloking44 <solop8483@gmail.com>
2024-08-28 08:01:26 +0000

    4-redis_advanced_op.js

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,22 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (error) => {
+  console.log(`Redis client connected to the server: ${error}`);
+});
+
+client.on('ready', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.hset('HolbertonSchools', 'Portland', '50', print);
+client.hset('HolbertonSchools', 'Seattle', '80', print);
+client.hset('HolbertonSchools', 'New York', '20', print);
+client.hset('HolbertonSchools', 'Bogota', '20', print);
+client.hset('HolbertonSchools', 'Cali', '40', print);
+client.hset('HolbertonSchools', 'Paris', '2', print);
+
+client.hgetall('HolbertonSchools', (error, obj) => {
+  console.log(obj);
+});
