de9cf08c7f2bcdbbd472b9dd0139d403ae4dc9a1
soloking44 <solop8483@gmail.com>
2024-08-28 08:05:30 +0000

    5-subscriber.js

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (error) => {
+  console.log(`Redis client connected to the server: ${error}`);
+});
+
+client.on('ready', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('message', (channel, message) => {
+  if (channel.localeCompare('holberton school channel') === 0) {
+    console.log(message);
+
+    if (message.localeCompare('KILL_SERVER') === 0) {
+      client.unsubscribe(channel);
+      client.quit();
+    }
+  }
+});
+
+client.subscribe('holberton school channel');
